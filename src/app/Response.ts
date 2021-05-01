74cb85423e0318e9c1fed4a082b13bcb5681c469
Shin Maung Maung <ethereal97@gmail.com>
2021-05-01 23:00:24 +0630

    a

--- src/app/Response.ts
+++ src/app/Response.ts
@@ -2,6 +2,19 @@ import User from "./User";
 
 type SenderAction = "typing_on" | "typing_off" | "mark_seen";
 
+interface QuickReply {
+  content_type?: "text" | "user_phone_number" | "user_email";
+  title?: string;
+  payload?: string;
+}
+
+interface Button {
+  content_type?: "web_url" | "postback" | "phone_number";
+  title?: string;
+  subtitle?: string;
+  image_url?: string;
+}
+
 export default class Response {
   static genTypingAction() {
     return [
@@ -17,24 +30,29 @@ export default class Response {
     return response;
   }
 
-  static genQuickReply(text, quickReplies) {
+  static genQuickReply(text: string, quickReplies: QuickReply[]) {
     let response = {
       text: text,
       quick_replies: [],
     };
 
-    for (let quickReply of quickReplies) {
-      response["quick_replies"].push({
-        content_type: "text",
-        title: quickReply["title"],
-        payload: quickReply["payload"],
+    for (let reply of quickReplies) {
+      response.quick_replies.push({
+        content_type: reply.content_type || "text",
+        title: reply.title,
+        payload: reply.payload,
       });
     }
 
     return response;
   }
 
-  static genGenericTemplate(image_url, title, subtitle, buttons) {
+  static genGenericTemplate(
+    image_url: string,
+    title: string,
+    subtitle: string,
+    buttons: Button[]
+  ) {
     let response = {
       attachment: {
         type: "template",
@@ -42,10 +60,10 @@ export default class Response {
           template_type: "generic",
           elements: [
             {
-              title: title,
-              subtitle: subtitle,
-              image_url: image_url,
-              buttons: buttons,
+              title,
+              image_url,
+              subtitle,
+              buttons,
             },
           ],
         },
@@ -55,7 +73,7 @@ export default class Response {
     return response;
   }
 
-  static genImageTemplate(image_url, title, subtitle = "") {
+  static genImageTemplate(image_url: string, title: string, subtitle?: string) {
     let response = {
       attachment: {
         type: "template",
@@ -63,9 +81,9 @@ export default class Response {
           template_type: "generic",
           elements: [
             {
-              title: title,
-              subtitle: subtitle,
-              image_url: image_url,
+              title,
+              image_url,
+              subtitle: subtitle || "",
             },
           ],
         },
@@ -75,14 +93,14 @@ export default class Response {
     return response;
   }
 
-  static genButtonTemplate(title, buttons) {
+  static genButtonTemplate(title: string, buttons: Button[]) {
     let response = {
       attachment: {
         type: "template",
         payload: {
           template_type: "button",
           text: title,
-          buttons: buttons,
+          buttons,
         },
       },
     };
@@ -90,34 +108,34 @@ export default class Response {
     return response;
   }
 
-  static genText(text) {
+  static genText(text: string) {
     let response = {
-      text: text,
+      text,
     };
 
     return response;
   }
 
-  static genTextWithPersona(text, persona_id) {
+  static genTextWithPersona(text: string, persona_id: string) {
     let response = {
-      text: text,
-      persona_id: persona_id,
+      text,
+      persona_id,
     };
 
     return response;
   }
 
-  static genPostbackButton(title, payload) {
+  static genPostbackButton(title: string, payload: string) {
     let response = {
       type: "postback",
-      title: title,
-      payload: payload,
+      title,
+      payload,
     };
 
     return response;
   }
 
-  static genWebUrlButton(title, url) {
+  static genWebUrlButton(title: string, url: string) {
     let response = {
       type: "web_url",
       title: title,
@@ -129,15 +147,15 @@ export default class Response {
   }
 
   static genNuxMessage(user: User) {
-    let welcome = this.genText(`မင်္ဂလာပါ ${user.name}`);
-    let curation = this.genQuickReply("ဘာများကူညီပေးရမလဲဗျ။", [
-      {
-        title: "သတင်းယူရန်",
-        payload: "NEWS_GETTING",
-      },
+    let welcome = this.genText(`မင်္ဂလာပါ။`);
+    let curation = this.genQuickReply("ဘာများကူညီပေးရမလဲခင်ဗျာ။", [
       {
         title: "သတင်းပေး",
         payload: "NEWS_REPORTING",
+      },
+      {
+        title: "သတင်းယူ",
+        payload: "NEWS_GETTING",
       },
     ]);
 
