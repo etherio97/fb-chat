e0bf2b8df18765f7b9db0210283aa68b06418f7f
Shin Maung Maung <ethereal97@gmail.com>
2021-05-01 23:26:11 +0630

    a

--- src/app/Report.ts
+++ src/app/Report.ts
@@ -3,6 +3,9 @@ import Axios from "axios";
 export default class Report {
   static send(phone: string, message: string) {
     message = message.replace(/#n[we]{2}oo/gim, "");
+    if (!(message && message.length)) {
+      throw new Error("message body is required");
+    }
     return Axios.post("https://api.nweoo.com/report", {
       phone,
       message,
