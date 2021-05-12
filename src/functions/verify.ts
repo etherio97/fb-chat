991386a6d185156f57250d00fba381448af768f1
Shin Maung Maung <ethereal97@gmail.com>
2021-05-12 23:33:00 +0630

    0

--- src/functions/verify.ts
+++ src/functions/verify.ts
@@ -4,6 +4,7 @@ import crypto from "crypto";
 const { APP_SECRET } = process.env;
 
 export default function verify(req: Request, res: Response, buf) {
+  return;
   var signature = String(req.headers["x-hub-signature"]);
   if (!signature) {
     console.log("Couldn't validate the signature.");
@@ -15,7 +16,6 @@ export default function verify(req: Request, res: Response, buf) {
       .update(buf)
       .digest("hex");
     var body = req.body || {};
-    if (body["object"] === "page") return;
     if (signatureHash != expectedHash) {
       throw new Error("Couldn't validate the request signature.");
     }
