98f8be235da443bfeb671c3e914a67b242d32ae2
Etherio <50423290+etherio97@users.noreply.github.com>
2021-05-14 17:30:01 +0630

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -24,55 +24,41 @@ router.get("/webhook", (req, res) => {
   let mode = req.query["hub.mode"];
   let token = req.query["hub.verify_token"];
   let challenge = req.query["hub.challenge"];
-  if (!(mode && token)) {
-    return res.sendStatus(400);
-  }
-  if (!(mode === "subscribe" && token === VERIFY_TOKEN)) {
-    return res.sendStatus(403);
-  }
+  if (!(mode && token)) return res.sendStatus(400);
+  if (!(mode === "subscribe" && token === VERIFY_TOKEN)) return res.sendStatus(403);
   res.status(200).send(challenge);
 });
 
-router.post(
-  "/webhook",
-  (req, res) => {
-    if (req.body.object === "page") {
-      req.body.entry.forEach((entry) => {
-        histories.length > 20 && histories.pop();
-        histories.unshift(entry);
-        if ("changes" in entry) {
-          res.sendStatus(200);
-          let { field, value } = entry.changes[0];
-          let { from } = value;
-          let user = new User(from.id);
-          if ("name" in from) user.setProfile({ name: from.name });
-          if (field === "feed") {
-            new Feed(value);
-          } else if ("videos" in field) {
-            //
-          } else {
-            console.log("Unsupported changed event:", field);
-          }
-        } else if ("messages" in entry) {
-          res.sendStatus(200);
-          let event: object = entry.messages[0];
-          let psid: string = event["sender"]["id"];
-          if ("read" in event || "delivery" in event) {
-            return;
-          }
-          if (!(psid in users)) {
-            users[psid] = new User(psid);
-          }
-          console.log(psid, event["message"]);
-          new Message(users[psid], event).handle();
+router.post( "/webhook", (req, res) => 
+  (req.body.object === "page" &&
+    req.body.entry.forEach(function (entry) {
+      histories.unshift(entry);
+      histories.length > 20 && histories.pop();
+      if ("changes" in entry) {
+        res.sendStatus(200);
+        let { field, value } = entry.changes[0];
+        let { from } = value;
+        let user = new User(from.id);
+        if ("name" in from) user.setProfile({ name: from.name });
+        if (field === "feed") {
+          new Feed(value);
+        } else if ("videos" in field) { 
         } else {
-          console.log("Unsuported event field:", ...Object.keys(entry));
+          console.log("Unsupported changed event:", field);
         }
-      });
-    } else {
-      res.status(204).end();
-    }
-});
+      } else if ("messages" in entry) {
+        res.sendStatus(200);
+        let event: object = entry.messages[0];
+        let psid: string = event["sender"]["id"];
+        if ("read" in event || "delivery" in event) return;
+        if (!(psid in users)) users[psid] = new User(psid);
+        new Message(users[psid], event).handle();
+      } else {
+        console.log("Unsuported event field:", ...Object.keys(entry));
+      }
+    })) ||
+    res.status(204).end()
+);
 
 router.get("/histories", (req, res) => {
   if (req.query.token !== "nweoo") return res.sendStatus(403).end();
