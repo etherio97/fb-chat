90de632305f1beb0c8c70eee475c5f6c178e4f05
Shin Maung Maung <ethereal97@gmail.com>
2025-07-13 19:27:51 +0700

    xxxx

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { config } from "dotenv";
+// import { config } from "dotenv";
 import express, { json, urlencoded } from "express";
 import cors from "cors";
 import DB from "./app/DB";
 import router from "./routes";
 
-config();
+// config();
 
 const app = express();
 const { PORT } = process.env;
@@ -13,7 +13,7 @@ DB.init();
 
 app.use(json());
 app.use(urlencoded({ extended: true }));
-app.use(cors())
+app.use(cors());
 
 app.use(router);
 
